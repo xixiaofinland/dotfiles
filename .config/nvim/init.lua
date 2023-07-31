f571dd7e100a116018ca418488f705d472139f09
Xi Xiao <ext-xi.xiao@elisa.fi>
2023-07-31 13:28:55 +0300

    +

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -159,6 +159,9 @@ require 'keys'
 require 'opts'
 require 'plug-settings'
 
+-- load some legacy vim scripts
+vim.cmd('source scripts.vim')
+
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
