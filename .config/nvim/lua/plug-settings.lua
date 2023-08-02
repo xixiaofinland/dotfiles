e0a7ab7dc857c114a1fccec7e8afced2cb06a629
xi xiao <tdxiaoxi2@gmail.com>
2023-08-02 19:44:08 +0300

    +

--- .config/nvim/lua/plug-settings.lua
+++ .config/nvim/lua/plug-settings.lua
@@ -2,8 +2,8 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        -- null_ls.builtins.diagnostics.eslint,
-        -- null_ls.builtins.completion.spell,
+        -- global prettier + local prettier-plugin-apex
+        -- make sure the package.json has correct version saved
         null_ls.builtins.formatting.prettier.with({
             filetypes = { "apex" },
             extra_args = { "--plugin=prettier-plugin-apex", "--write" },
