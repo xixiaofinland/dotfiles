122ce78f0f957f8d6e3741c08ab76b9ac2ae69af
Xi Xiao <ext-xi.xiao@elisa.fi>
2023-08-02 10:21:23 +0300

    +

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -36,8 +36,8 @@ vim.api.nvim_set_keymap(
 
 -- Salesforce keys
 
-vim.keymap.set('n', '<leader>sfp',
-  '<C-w>s<C-w>j10<C-w>-:term sf project deploy start  --source-dir "%" --target-org xixiao100<CR>',
-  { desc = 'Apex deploy current file' })
+-- vim.keymap.set('n', '<leader>sfp',
+--   '<C-w>s<C-w>j10<C-w>-:term sf project deploy start  --source-dir "%" --target-org xixiao100<CR>',
+--   { desc = 'Apex deploy current file' })
 
 -- vim.keymap.set('n', '<leader>fa', '<C-w>s<C-w>j10<C-w>-:term prettier --plugin=prettier-plugin-apex --write "%" <CR>', { desc = 'Format by cmd' })
