2b50b249531e5085a309686e0401d212083c42ab
Xi Xiao <ext-xi.xiao@elisa.fi>
2023-08-17 15:10:25 +0300

    +

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -70,4 +70,8 @@ vim.keymap.set('n', '<leader>sfp',
   '<C-w>s<C-w>j10<C-w>-:term sf project deploy start  --source-dir "%" --target-org xixiao100<CR>',
   { desc = 'Apex deploy current file' })
 
+vim.keymap.set('n', '<leader>sff',
+  ':!npm run prettier -- --plugin=prettier-plugin-apex --write "%"<CR>',
+  { desc = 'Prettier local format' })
+
 -- vim.keymap.set('n', '<leader>fa', '<C-w>s<C-w>j10<C-w>-:term prettier --plugin=prettier-plugin-apex --write "%" <CR>', { desc = 'Format by cmd' }
