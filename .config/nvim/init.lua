624ded94c2265d35b661f54907f9d3c38122f1f0
xi xiao <tdxiaoxi2@gmail.com>
2023-08-27 18:47:36 +0300

    +

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,9 +84,11 @@ require('lazy').setup({
         vim.keymap.set('n', ']c', require('gitsigns').next_hunk, { buffer = bufnr, desc = 'next hunk' })
         vim.keymap.set('n', '<leader>hh', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'preview hunk' })
         vim.keymap.set('n', '<leader>ha', require('gitsigns').stage_hunk, { buffer = bufnr, desc = 'stage_hunk' })
-        vim.keymap.set('n', '<leader>hu', require('gitsigns').undo_stage_hunk, { buffer = bufnr, desc = 'undo_stage_hunk' })
+        vim.keymap.set('n', '<leader>hu', require('gitsigns').undo_stage_hunk,
+          { buffer = bufnr, desc = 'undo_stage_hunk' })
         vim.keymap.set('n', '<leader>hS', require('gitsigns').stage_buffer, { buffer = bufnr, desc = 'stage_buffer' })
-        vim.keymap.set('n', '<leader>hb', require('gitsigns').toggle_current_line_blame, { buffer = bufnr, desc = 'toggle line blame' })
+        vim.keymap.set('n', '<leader>hb', require('gitsigns').toggle_current_line_blame,
+          { buffer = bufnr, desc = 'toggle line blame' })
       end,
     },
   },
