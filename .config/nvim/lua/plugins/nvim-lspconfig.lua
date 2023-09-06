663d564ff62c90615a27d10241dfe397cd9e3ecb
xi xiao <tdxiaoxi2@gmail.com>
2023-09-06 20:56:18 +0300

    +

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,14 +2,14 @@ return {
   'neovim/nvim-lspconfig',
   dependencies = {
     -- Automatically install LSPs to stdpath for neovim
-    { 'williamboman/mason.nvim', config = true,opts = {} },
+    { 'williamboman/mason.nvim', config = true,  opts = {} },
     'williamboman/mason-lspconfig.nvim',
 
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
 
     -- Additional lua configuration, makes nvim stuff amazing!
-    {'folke/neodev.nvim', opts = {}},
+    { 'folke/neodev.nvim',       opts = {} },
   },
   config = function()
     -- [[ Configure LSP ]]
@@ -24,18 +24,14 @@ return {
 
       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
       nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
       nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
       nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
       nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
       nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
       nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
       nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-      -- See `:help K` for why this keymap
       nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
       nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
       -- Lesser used LSP functionality
       nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
       nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
@@ -64,11 +60,7 @@ return {
           telemetry = { enable = false },
         },
       },
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
       -- apex_ls = {},
-      -- tsserver = {},
     }
 
     -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
@@ -97,8 +89,8 @@ return {
     lspconfig.apex_ls.setup {
       -- Note. need manually to download apex LSP jar
       apex_jar_path = os.getenv('HOME') .. '/apex-jorje-lsp.jar',
-      apex_enable_semantic_errors = false,       -- Whether to allow Apex Language Server to surface semantic errors
-      apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
+      apex_enable_semantic_errors = false,
+      apex_enable_completion_statistics = false,
       filetypes = { 'apex' },
       root_dir = lspconfig.util.root_pattern('sfdx-project.json'),
 
