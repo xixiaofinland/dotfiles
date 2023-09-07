6fb11b2c0b084b355b414237430178a41efee9ea
xi xiao <tdxiaoxi2@gmail.com>
2023-09-07 20:57:03 +0300

    +

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -100,3 +100,9 @@ vim.g.loaded_rrhelper = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
