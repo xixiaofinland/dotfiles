0f4e60c684040044debb3adeb26f9c952a5c860a
xi xiao <tdxiaoxi2@gmail.com>
2023-09-09 23:53:21 +0300

    +

--- .config/nvim/lua/plugins/nvim-tree-sitter.lua
+++ .config/nvim/lua/plugins/nvim-tree-sitter.lua
@@ -29,19 +29,20 @@ return {
           enable = true,
           set_jumps = true, -- whether to set jumps in the jumplist
           goto_next_start = {
-            [']m'] = '@function.outer',
+            [']f'] = '@function.outer',
             [']]'] = '@class.outer',
           },
           goto_next_end = {
-            [']M'] = '@function.outer',
+            [']g'] = '@function.outer',
             [']['] = '@class.outer',
           },
           goto_previous_start = {
-            ['[m'] = '@function.outer',
+            ['[f'] = '@function.outer',
             ['[['] = '@class.outer',
           },
           goto_previous_end = {
-            ['[M'] = '@function.outer',
+            ['[F'] = '@function.outer',
+
             ['[]'] = '@class.outer',
           },
         },
