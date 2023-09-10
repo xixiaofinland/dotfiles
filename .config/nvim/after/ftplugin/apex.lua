e54c41cf5427b628a8b786693ee998ade96ce80e
xi xiao <tdxiaoxi2@gmail.com>
2023-09-10 12:26:15 +0300

    +

--- .config/nvim/after/ftplugin/apex.lua
+++ .config/nvim/after/ftplugin/apex.lua
@@ -14,5 +14,5 @@ vim.keymap.set('n', '<leader>sk',
   ':!npm run prettier -- --plugin=prettier-plugin-apex --write "%"<CR>',
   { desc = 'Prettier current file' })
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
