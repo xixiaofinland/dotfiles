50c41efd76eabe3d0767fdfb2a917cc46ea76b8b
Xi Xiao <ext-xi.xiao@elisa.fi>
2023-09-12 17:34:51 +0300

    +

--- .config/nvim/after/queries/apex/injections.scm
+++ .config/nvim/after/queries/apex/injections.scm
@@ -1,3 +1,3 @@
 ((soql_query) @injection.content
-      (#set! injection.language "soql")) ; set the parser language for @injection.content region to javascript
+      (#set! injection.language "soql"))
 
