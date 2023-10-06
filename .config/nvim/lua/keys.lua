c8075241a44e2c8744ce44bcf10c9f9c4c6ac19c
xi xiao <tdxiaoxi2@gmail.com>
2023-10-06 19:59:16 +0300

    +

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -1,6 +1,6 @@
-vim.keymap.set('n', '<leader>t', '<CMD>lua require("sfterm").toggle()<CR>')
-vim.keymap.set('n', '<leader>sp', '<CMD>lua require("sfterm").push()<CR>')
-vim.keymap.set('n', '<leader>sr', '<CMD>lua require("sfterm").retrieve()<CR>')
+vim.keymap.set('n', '<leader>t', require("sfterm").toggle, { desc = "Toggle terminal" })
+vim.keymap.set('n', '<leader>sp', require("sfterm").push,  { desc = "Sf: push current file" })
+vim.keymap.set('n', '<leader>sr', require("sfterm").retrieve,  { desc = "Sf: retrieve current file" })
 
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
