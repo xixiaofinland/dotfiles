dc41d5d251cdab4d5409a4b0efd73173fe92b5c5
xi xiao <tdxiaoxi2@gmail.com>
2023-10-07 17:07:37 +0300

    +

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -42,11 +42,3 @@ vim.keymap.set('n', '[<Space>', ':<C-u>put!=repeat(nr2char(10),v:count)<Bar>exec
   { silent = true, noremap = true })
 
 vim.keymap.set('n', '<leader>b', ':b#|bd#<CR>', { noremap = true, desc = 'Close current buffer' })
-
--- vim.keymap.set("n", "<leader>ht", require("harpoon.ui").toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
--- vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file, { desc = "Add file to harpoon list" })
-for pos = 0, 9 do
-  vim.keymap.set("n", "<leader>h" .. pos, function()
-    require("harpoon.ui").nav_file(pos)
-  end, { desc = "Move to harpoon mark #" .. pos })
-end
--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -3,5 +3,10 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>ht", require("harpoon.ui").toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
     vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file, { desc = "Add file to harpoon list" })
+    for pos = 0, 9 do
+      vim.keymap.set("n", "<leader>h" .. pos, function()
+        require("harpoon.ui").nav_file(pos)
+      end, { desc = "Move to harpoon mark #" .. pos })
+    end
   end
 }
