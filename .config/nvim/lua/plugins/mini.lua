0becc111ead8a81d62b81adb8e0c8c216a14ed8d
xi xiao <tdxiaoxi2@gmail.com>
2023-10-14 20:06:23 +0300

    +

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -6,6 +6,7 @@ return {
       require('mini.surround').setup()
       require('mini.cursorword').setup()
       require('mini.ai').setup()
+      require('mini.pairs').setup()
       require('mini.bufremove').setup({
         silent = true,
         vim.keymap.set('n', '<leader>b', require'mini.bufremove'.delete, { desc = 'remove current buffer' })
