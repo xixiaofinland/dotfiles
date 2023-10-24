11229aaf086b96299d3b3da2a6d90071525ff769
xi xiao <tdxiaoxi2@gmail.com>
2023-10-25 02:26:06 +0300

    +

--- .config/nvim/lua/plugins/sforg.lua
+++ .config/nvim/lua/plugins/sforg.lua
@@ -6,7 +6,8 @@ return {
   -- },
 
   config = function()
-    vim.keymap.set('n', '<leader>ss', require("sforg").set, { desc = "set target_org" })
+    vim.keymap.set('n', '<leader>ss', require("sforg").set, { desc = "set target_org for the current workspace" })
+    vim.keymap.set('n', '<leader>sS', require("sforg").setGlobal, { desc = "set target_org globally" })
     vim.keymap.set('n', '<leader>sf', require("sforg").fetch, { desc = "fetch orgs info" })
   end
 
