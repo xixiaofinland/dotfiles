d05cb82f082926cce31f015f8cc3254224c2c2cb
xi xiao <tdxiaoxi2@gmail.com>
2023-12-02 17:11:35 +0200

    +

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,13 @@ return {
   {
     'stevearc/oil.nvim',
     opts = {},
+
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
+
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", "<CMD>Oil --float <CR>", { desc = "Open parent directory" })
+    end
   }
 }
