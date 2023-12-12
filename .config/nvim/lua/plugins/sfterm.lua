5e6cffc0816b33328848fad98ec41713d9de6811
xi xiao <tdxiaoxi2@gmail.com>
2023-12-12 12:00:30 +0200

    +

--- .config/nvim/lua/plugins/sfterm.lua
+++ .config/nvim/lua/plugins/sfterm.lua
@@ -5,7 +5,7 @@ return {
   'xixiaofinland/sfterm.nvim',
   config = function()
     vim.keymap.set('n', '<leader>t', require("sfterm").toggle, { desc = "Toggle terminal" })
-    vim.keymap.set('n', '<leader>sp', require("sfterm").push, { desc = "Sf: push current file" })
+    vim.keymap.set('n', '<leader>sp', require("sfterm").saveAndPush, { desc = "Sf: push current file" })
     vim.keymap.set('n', '<leader>sr', require("sfterm").retrieve, { desc = "Sf: retrieve current file" })
   end
   -- },
