d417694720c4f1f514c91465a7f7a092bbeb6afb
xi xiao <tdxiaoxi2@gmail.com>
2023-12-12 13:52:36 +0200

    +

--- .config/nvim/lua/plugins/null_ls.lua
+++ .config/nvim/lua/plugins/null_ls.lua
@@ -5,11 +5,10 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        -- not work global prettier :(
-        -- null_ls.builtins.formatting.prettier.with({
-        --     filetypes = { "apex" },
-        --     extra_args = { "--plugin=prettier-plugin-apex", "--write" },
-        -- }),
+        null_ls.builtins.formatting.prettier.with({
+            filetypes = { "apex" },
+            extra_args = { "--plugin=prettier-plugin-apex", "--write" },
+        }),
 
         null_ls.builtins.diagnostics.pmd.with({
           filetypes = { "apex" },
