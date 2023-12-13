c28d4b775144ecb0aa627165fb31b799907b8b04
xi xiao <tdxiaoxi2@gmail.com>
2023-12-13 21:31:32 +0200

    +

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -59,7 +59,9 @@ return {
     end, { desc = '[/] Fuzzily search in current buffer' })
 
     vim.keymap.set("n", "<leader>fs", tel.lsp_document_symbols, { desc = 'List [S]ymbols' })
-    vim.keymap.set("n", "<leader>fm", tel.treesitter, { desc = 'List [M]ethods' })
+    vim.keymap.set("n", "<leader>fm", function() tel.lsp_document_symbols({ symbols = { 'function', 'method' } }) end,
+      { desc = 'List [M]ethods' })
+    vim.keymap.set("n", "<leader>ft", tel.treesitter, { desc = 'List [T]reesitter symbols' })
     vim.keymap.set('n', '<leader>fi', tel.git_files, { desc = '[F]ind g[I]t files' })
     vim.keymap.set('n', '<leader>ff', tel.find_files, { desc = '[F]ind [F]iles' })
     vim.keymap.set('n', '<leader>fh', tel.help_tags, { desc = '[F]ind [H]elp' })
