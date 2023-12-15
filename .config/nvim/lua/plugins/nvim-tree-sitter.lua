a2d917eeb6f2d99870ec9337df38b05d71560e3e
xi xiao <tdxiaoxi2@gmail.com>
2023-12-15 17:22:54 +0200

    +

--- .config/nvim/lua/plugins/nvim-tree-sitter.lua
+++ .config/nvim/lua/plugins/nvim-tree-sitter.lua
@@ -117,11 +117,10 @@ return {
       },
     })
 
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
+    -- local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
     -- vim way: ; goes to the direction you were moving.
-    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+    -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+    -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
     -- they conflict with "." action in vim, do I really need them?
     -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
