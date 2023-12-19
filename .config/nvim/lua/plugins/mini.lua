434a4961c77ad6f9188bc767529323ff155bb9c6
xi xiao <tdxiaoxi2@gmail.com>
2023-12-19 11:22:35 +0200

    +

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -4,12 +4,27 @@ return {
     version = false,
     config = function()
       require('mini.surround').setup()
-      -- require('mini.cursorword').setup()
-      require('mini.ai').setup()
-      -- require('mini.pairs').setup()
+
+      local ai = require("mini.ai");
+      ai.setup({
+        custom_textobjects = {
+          a = ai.gen_spec.argument({ brackets = { '%b()' } }),
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }, {}),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+        },
+      })
+
       require('mini.bufremove').setup({
         silent = true,
       })
+
+      -- require('mini.pairs').setup()
+      -- require('mini.cursorword').setup()
+
       vim.keymap.set('n', '<leader>b', require 'mini.bufremove'.delete, { desc = 'remove current buffer' })
     end,
   }
