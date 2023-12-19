fba5f57d349e5151455762fc3ce42c1d2c55bd3e
xi xiao <tdxiaoxi2@gmail.com>
2023-12-19 16:58:26 +0200

    +

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -5,18 +5,18 @@ return {
     config = function()
       require('mini.surround').setup()
 
-      local ai = require("mini.ai");
-      ai.setup({
+      local gen_spec = require("mini.ai").gen_spec;
+      require("mini.ai").setup({
         custom_textobjects = {
           -- match only within `()`
-          a = ai.gen_spec.argument({ brackets = { '%b()' } }),
+          a = gen_spec.argument({ brackets = { '%b()' } }),
 
-          n = ai.gen_spec.treesitter({ a = "@meth_name", i = "@meth_name" }, {}),
-          m = ai.gen_spec.treesitter({ a = '@call.outer', i = '@call.inner' }),
-          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+          n = gen_spec.treesitter({ a = "@meth_name", i = "@meth_name" }, {}),
+          m = gen_spec.treesitter({ a = '@call.outer', i = '@call.inner' }),
+          f = gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+          c = gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
 
-          -- select entire buffer
+          -- entire buffer
           g = function()
             local from = { line = 1, col = 1 }
             local to = {
