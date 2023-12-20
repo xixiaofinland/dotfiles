2e51702d06a1aed2aff33b4db74177041a3ad513
xi xiao <tdxiaoxi2@gmail.com>
2023-12-20 16:59:17 +0200

    +

--- .config/nvim/lua/plugins/nvim-tree-sitter.lua
+++ .config/nvim/lua/plugins/nvim-tree-sitter.lua
@@ -14,16 +14,6 @@ return {
         additional_vim_regex_highlighting = false,
       },
 
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
-        },
-      },
-
       textobjects = {
         select = {
           enable = true,
