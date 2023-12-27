7126c51f175151cf9c1d66f459cfdeca19999823
xi xiao <tdxiaoxi2@gmail.com>
2023-12-28 01:12:12 +0200

    +

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,15 @@
 return {
-  "sainnhe/gruvbox-material",
-  priority = 1000,
+  -- "sainnhe/gruvbox-material",
+  -- priority = 1000,
+  -- config = function()
+  --   vim.o.background = "dark" -- or "light" for light mode
+  --   vim.cmd.colorscheme 'gruvbox-material'
+  -- end,
+
+  "rebelot/kanagawa.nvim",
   config = function()
-    vim.o.background = "dark" -- or "light" for light mode
-    vim.cmd.colorscheme 'gruvbox-material'
-  end,
+    vim.cmd("colorscheme kanagawa-wave")
+    -- vim.cmd("colorscheme kanagawa-dragon")
+    -- vim.cmd("colorscheme kanagawa-lotus")
+  end
 }
