1dd9c1d5f38367a06bededd3ae801463c4640db2
xi xiao <tdxiaoxi2@gmail.com>
2024-01-03 21:16:33 +0200

    +

---
+++ .config/nvim/lua/plugins/sf.lua
@@ -0,0 +1,20 @@
+return {
+  'xixiaofinland/sf.nvim',
+  config = function()
+    local nmap = function(keys, func, desc)
+      if desc then
+        desc = '[Sf] ' .. desc
+      end
+      vim.keymap.set('n', keys, func, { desc = desc })
+    end
+
+    nmap('<leader>ss', require("sf.org").set, "[s]et target_org current workspace")
+    nmap('<leader>sS', require("sf.org").setGlobal, "[S]et target_org globally")
+    nmap('<leader>sf', require("sf.org").fetch, "[F]etch orgs info")
+
+    nmap('<leader><leader>t', require("sf.term").toggle, "[T]erminal toggle")
+    nmap('<leader>sp', require("sf.term").saveAndPush, "[P]ush current file")
+    nmap('<leader>sr', require("sf.term").retrieve, "[R]etrieve current file")
+  end
+  -- },
+}
