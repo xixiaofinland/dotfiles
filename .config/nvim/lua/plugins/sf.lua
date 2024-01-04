38df03b1f5d5e6fe73f13ead4b1fe727abed23e0
xi xiao <tdxiaoxi2@gmail.com>
2024-01-04 22:09:04 +0200

    +

--- .config/nvim/lua/plugins/sf.lua
+++ .config/nvim/lua/plugins/sf.lua
@@ -16,6 +16,9 @@ return {
     nmap('<leader>sp', require("sf.term").saveAndPush, "[P]ush current file")
     nmap('<leader>sr', require("sf.term").retrieve, "[R]etrieve current file")
 
+    local sf_run_all_test = ''
+    nmap('<leader>sta', function() require("sf.term").run('echo hello') end, "[T]est [A]ll")
+
     vim.api.nvim_create_autocmd({ "VimEnter" }, {
       callback = function()
         require("sf.org").fetch()
