b33911d9cfff3d7f82e7d286fbf5853ad0211985
xi xiao <tdxiaoxi2@gmail.com>
2024-01-06 12:40:07 +0200

    +

--- .config/nvim/lua/plugins/sf.lua
+++ .config/nvim/lua/plugins/sf.lua
@@ -8,15 +8,9 @@ return {
       vim.keymap.set('n', keys, func, { desc = desc })
     end
 
-    nmap('<leader>ss', require("sf.org").set, "[s]et target_org current workspace")
-    nmap('<leader>sS', require("sf.org").setGlobal, "[S]et target_org globally")
-    nmap('<leader>sf', require("sf.org").fetch, "[F]etch orgs info")
-
     nmap('<leader><leader>t', require("sf.term").toggle, "[T]erminal toggle")
-    nmap('<leader>sp', require("sf.term").saveAndPush, "[P]ush current file")
-    nmap('<leader>sr', require("sf.term").retrieve, "[R]etrieve current file")
-    nmap('<leader>sta', require("sf.term").runAllTestsInCurrentFile, "[T]est [A]ll run")
-    nmap('<leader>stt', require("sf.term").runCurrentTest, "[T]est [T]his run")
+
+    -- other keys defined in after/ftplugins/apex.lua
 
     vim.api.nvim_create_autocmd({ "VimEnter" }, {
       callback = function()
@@ -24,5 +18,4 @@ return {
       end,
     })
   end
-  -- },
 }
