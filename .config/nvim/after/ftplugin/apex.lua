a39bb85d2b22527baa7d36df26eb249c99ecc047
xi xiao <tdxiaoxi2@gmail.com>
2024-01-14 21:12:52 +0200

    +

--- .config/nvim/after/ftplugin/apex.lua
+++ .config/nvim/after/ftplugin/apex.lua
@@ -12,9 +12,15 @@ nmap('<leader>sS', require("sf.org").setGlobal, "[S]et target_org globally")
 nmap('<leader>sf', require("sf.org").fetch, "[F]etch orgs info")
 
 nmap('<leader>t', require("sf.term").toggle, "[T]erminal toggle")
+
 nmap('<leader>sp', require("sf.term").saveAndPush, "[P]ush current file")
 nmap('<leader>sr', require("sf.term").retrieve, "[R]etrieve current file")
+
 nmap('<leader>sc', require("sf.term").cancel, "[C]ancel current running command")
 nmap('<leader>se', require("sf.term").scrollToEnd, "[E]nd scroll in terminal")
+
 nmap('<leader>sta', require("sf.term").runAllTestsInCurrentFile, "[T]est [A]ll")
 nmap('<leader>stt', require("sf.term").runCurrentTest, "[T]est [T]his under cursor")
+
+nmap('<leader>sto', require("sf.ts").open_test_buf, "[T]est [O]pen Buf Select")
+nmap('<leader>str', require("sf.term").repeatLastTests, "[T]est [R]epeat")
