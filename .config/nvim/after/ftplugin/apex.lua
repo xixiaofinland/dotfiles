b00b59527403b7f911a0a06220cf00931051b205
xi xiao <tdxiaoxi2@gmail.com>
2024-01-16 00:45:49 +0200

    +

--- .config/nvim/after/ftplugin/apex.lua
+++ .config/nvim/after/ftplugin/apex.lua
@@ -11,7 +11,7 @@ nmap('<leader>ss', require("sf.org").set, "[s]et target_org current workspace")
 nmap('<leader>sS', require("sf.org").setGlobal, "[S]et target_org globally")
 nmap('<leader>sf', require("sf.org").fetch, "[F]etch orgs info")
 
-nmap('<leader>t', require("sf.term").toggle, "[T]erminal toggle")
+nmap('<leader><leader>', require("sf.term").toggle, "[T]erminal toggle")
 
 nmap('<leader>sp', require("sf.term").saveAndPush, "[P]ush current file")
 nmap('<leader>sr', require("sf.term").retrieve, "[R]etrieve current file")
@@ -19,8 +19,8 @@ nmap('<leader>sr', require("sf.term").retrieve, "[R]etrieve current file")
 nmap('<C-c>', require("sf.term").cancel, "[C]ancel current running command")
 nmap('<leader>se', require("sf.term").scrollToEnd, "[E]nd scroll in terminal")
 
-nmap('<leader>sta', require("sf.term").runAllTestsInCurrentFile, "[T]est [A]ll")
-nmap('<leader>stt', require("sf.term").runCurrentTest, "[T]est [T]his under cursor")
+nmap('<leader>ta', require("sf.term").runAllTestsInCurrentFile, "[T]est [A]ll")
+nmap('<leader>tt', require("sf.term").runCurrentTest, "[T]est [T]his under cursor")
 --
-nmap('<leader>sto', require("sf.test").open, "[T]est [O]pen Buf Select")
-nmap('<leader>str', require("sf.term").repeatLastTests, "[T]est [R]epeat")
+nmap('<leader>to', require("sf.test").open, "[T]est [O]pen Buf Select")
+nmap('<leader>tr', require("sf.term").repeatLastTests, "[T]est [R]epeat")
