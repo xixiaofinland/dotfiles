ef33f05fa19c42cc5bb370cf6c5dd5828c195494
xi xiao <tdxiaoxi2@gmail.com>
2024-01-28 13:02:16 +0200

    +

--- .config/nvim/lua/plugins/sf.lua
+++ .config/nvim/lua/plugins/sf.lua
@@ -2,13 +2,6 @@ return {
   'xixiaofinland/sf.nvim',
   dir = '~/projects/sf.nvim',
   config = function()
-    local nmap = function(keys, func, desc)
-      if desc then
-        desc = '[Sf] ' .. desc
-      end
-      vim.keymap.set('n', keys, func, { desc = desc })
-    end
-
     -- keys are defined in after/ftplugins/apex.lua
 
     vim.api.nvim_create_autocmd({ "VimEnter" }, {
