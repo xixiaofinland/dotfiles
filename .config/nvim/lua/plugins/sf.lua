c11d8e3fdd26545d41d0569f8d042778c2b0d6b4
xi xiao <tdxiaoxi2@gmail.com>
2024-02-02 23:09:47 +0200

    +

--- .config/nvim/lua/plugins/sf.lua
+++ .config/nvim/lua/plugins/sf.lua
@@ -9,11 +9,5 @@ return {
 
   config = function()
     -- keys are defined in after/ftplugins/apex.lua
-
-    -- vim.api.nvim_create_autocmd({ 'VimEnter' }, {
-    --   callback = function()
-    --     require('sf.org').fetch_org_list()
-    --   end,
-    -- })
   end
 }
