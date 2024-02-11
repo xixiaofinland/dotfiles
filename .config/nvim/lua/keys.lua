9f04e5b08053e969ecd00ac4e4e761b1f6499048
xi xiao <tdxiaoxi2@gmail.com>
2024-02-11 09:17:28 +0200

    +

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -1,5 +1,3 @@
-local name = "[Keys] "
-
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
@@ -13,7 +11,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = name .. 'paste without losing the copy' })
+vim.keymap.set("x", "<leader>p", [["_dP]], { 'paste without losing the copy' })
 
 -- test if this is needed in macOS?
 -- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
@@ -22,18 +20,19 @@ vim.keymap.set("x", "<leader>p", [["_dP]], { desc = name .. 'paste without losin
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 vim.keymap.set('n', ']<Space>', ':<C-u>put =repeat(nr2char(10),v:count)<Bar>execute "\'[-1"<CR>',
-  { desc = name .. 'add empty line below', silent = true, noremap = true })
+  { 'add empty line below', silent = true, noremap = true })
 vim.keymap.set('n', '[<Space>', ':<C-u>put!=repeat(nr2char(10),v:count)<Bar>execute "\']+1"<CR>',
-  { desc = name .. 'add empty line above', silent = true, noremap = true })
+  { 'add empty line above', silent = true, noremap = true })
 
 -- Diagnostic
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = name .. 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = name .. 'Open diagnostics list' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { 'Open diagnostics list' })
 
 vim.keymap.set('n', '<leader>fn', function() return ':e ' .. vim.fn.expand '%:p:h' .. '/' end,
-  { expr = true, desc = name .. '[N]ew the current file path in ex' })
+  { expr = true, '[N]ew the current file path in ex' })
 
+-- Copy current file name without section after `dot`, e.g. copy "Hello" from "Hello.cls"
 vim.keymap.set('n', '<leader>cc', function()
   local file_name = vim.split(vim.fn.expand("%:t"), ".", { trimempty = true, plain = true })[1]
   vim.fn.setreg('*', file_name)
-end, { desc = name .. '[C]opy file_name' })
+end, { '[C]opy file_name' })
