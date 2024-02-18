1f70a264d95b437b5270dfbd08ce3c5d24cf9fa7
xi xiao <tdxiaoxi2@gmail.com>
2024-02-18 17:22:38 +0200

    +

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -30,7 +30,7 @@ return {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete {},
-        ['C-y'] = cmp.mapping.confirm {
+        ['<CR>'] = cmp.mapping.confirm {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         },
