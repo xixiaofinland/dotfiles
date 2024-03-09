3d272843b7809320ec50349973aede511e694f74
xi xiao <tdxiaoxi2@gmail.com>
2024-03-09 11:29:31 +0200

    +

--- .config/nvim/lua/plugins/sf.lua
+++ .config/nvim/lua/plugins/sf.lua
@@ -9,6 +9,26 @@ return {
   },
 
   config = function()
-    require('sf').setup() -- important to call setup() to init the plugin!
+    require('sf').setup({
+
+      -- Hotkeys and user commands are enabled for these filetypes
+      -- hotkeys_in_filetypes = {
+      --   "apex", "sosl", "soql", "javascript", "html"
+      -- },
+
+      -- When set to `true`, hotkeys and user commands are only enabled when the file
+      -- resides in a sf project folder (i.e. has `.forceignore` or `sfdx-project.json` in the root path)
+      -- When set to `false`, filetypes defined in `hotkeys_in_filetypes` have
+      -- hotkeys and user commands enabled.
+      enable_hotkeys_only_in_sf_project_folder = true,
+
+      -- Define what metadata file names to be listed in `list_md_to_retrieve()` (<leader>ml)
+      -- types_to_retrieve = {
+      --   "ApexClass",
+      --   "ApexTrigger",
+      --   "StaticResource",
+      --   "LightningComponentBundle"
+      -- },
+    })
   end
 }
