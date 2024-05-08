14c4f008bad76cfde9e45bd15d759733e712f0a3
xi xiao <tdxiaoxi2@gmail.com>
2024-05-08 22:31:02 +0300

    +

--- .config/nvim/data/plenary/filetypes/sf_type.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- fix the issue that telescope previewer doesn't code highlight these files below.
-  extension = {
-    ['cls'] = 'apex',
-    ['trigger'] = 'apex',
-    ['page'] = 'html',
-  }
-}
