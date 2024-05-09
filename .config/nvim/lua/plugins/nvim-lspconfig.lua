3f8b32909521be8f86692505b1cbaaf71361039d
xi xiao <tdxiaoxi2@gmail.com>
2024-05-09 20:06:09 +0300

    +

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -37,6 +37,7 @@ return {
       nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
       -- Lesser used LSP functionality
+      nmap('gD', vim.lsp.buf.definition, '[G]oto [d]efinition')
       nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
       nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
       nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
