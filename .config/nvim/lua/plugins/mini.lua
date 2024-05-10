dfdb67f4db56aefe2b12450c5969fa7b4d4fa304
xi xiao <tdxiaoxi2@gmail.com>
2024-05-10 23:36:29 +0300

    +

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -122,22 +122,6 @@ return {
           vim.keymap.set('n', 'g.', toggle_dotfiles, { buffer = buf_id })
         end,
       })
-
-      --  Create mapping to set current working directory ~
-
-      local files_set_cwd = function(path)
-        -- Works only if cursor is on the valid file system entry
-        local cur_entry_path = MiniFiles.get_fs_entry().path
-        local cur_directory = vim.fs.dirname(cur_entry_path)
-        vim.fn.chdir(cur_directory)
-      end
-
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'MiniFilesBufferCreate',
-        callback = function(args)
-          vim.keymap.set('n', 'g@', files_set_cwd, { buffer = args.data.buf_id })
-        end,
-      })
     end,
   }
 }
