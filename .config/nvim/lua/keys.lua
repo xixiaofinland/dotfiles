f9d086db342a5c35b97a6f9b2d9759c998001e89
xi xiao <tdxiaoxi2@gmail.com>
2024-05-11 19:20:00 +0300

    +

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -18,41 +18,31 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'paste without losing the copy' })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Paste without losing the copy' })
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 vim.keymap.set('n', ']<Space>', ':<C-u>put =repeat(nr2char(10),v:count)<Bar>execute "\'[-1"<CR>',
-  { desc = 'add empty line below', silent = true, noremap = true })
+  { desc = 'Add empty line below', silent = true, noremap = true })
 vim.keymap.set('n', '[<Space>', ':<C-u>put!=repeat(nr2char(10),v:count)<Bar>execute "\']+1"<CR>',
-  { desc = 'add empty line above', silent = true, noremap = true })
+  { desc = 'Add empty line above', silent = true, noremap = true })
 
 -- My own habit
 
 vim.keymap.set('n', '<leader>fn', function() return ':e ' .. vim.fn.expand '%:p:h' .. '/' end,
-  { expr = true, desc = '[N]ew the current file path in ex' })
+  { expr = true, desc = 'New a file' })
 
 vim.keymap.set('n', '<leader>cf', function()
   local file_name = vim.fn.expand("%:t")
   vim.fn.setreg('*', file_name)
   vim.notify(string.format('"%s" copied.', file_name), vim.log.levels.INFO)
-end, { desc = '[c]opy [f]ile name' })
+end, { desc = 'Copy file name' })
 
 vim.keymap.set('n', '<leader>cF', function()
   local file_name = vim.fn.expand("%:p")
   vim.fn.setreg('*', file_name)
   vim.notify(string.format('"%s" copied.', file_name), vim.log.levels.INFO)
-end, { desc = '[c]opy [f]ile name full path' })
+end, { desc = 'Copy File name full path' })
 
 vim.keymap.set('n', '<leader>e', '<CMD>e ~/.config/nvim/lua/plugins/sf.lua<CR>',
-  { desc = 'Open sf.nvim plugin config file' })
-
--- test if this is needed in macOS?
--- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
--- vim.keymap.set("n", "<leader>Y", [["+Y]])
-
--- seldomly used
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-
--- it equals to telescope <leader>d
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+  { desc = 'Open sf config' })
