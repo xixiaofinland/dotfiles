e63c97824fd1097c46192f77f446a14da31dcb46
xi xiao <tdxiaoxi2@gmail.com>
2024-05-14 21:35:04 +0300

    +

--- .config/nvim/lua/plugins/sf.lua
+++ .config/nvim/lua/plugins/sf.lua
@@ -1,7 +1,7 @@
 return {
   'xixiaofinland/sf.nvim',
   branch = 'dev',
-  -- dir = '~/projects/sf.nvim',
+  dir = '~/projects/sf.nvim',
 
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
@@ -10,25 +10,7 @@ return {
 
   config = function()
     require('sf').setup({
-
-      -- Hotkeys and user commands are enabled for these filetypes
-      -- hotkeys_in_filetypes = {
-      --   "apex", "sosl", "soql", "javascript", "html"
-      -- },
-
-      -- When set to `true`, hotkeys and user commands are only enabled when the file
-      -- resides in a sf project folder (i.e. has `.forceignore` or `sfdx-project.json` in the root path)
-      -- When set to `false`, filetypes defined in `hotkeys_in_filetypes` have
-      -- hotkeys and user commands enabled.
-      enable_hotkeys_only_in_sf_project_folder = true,
-
-      -- Define what metadata file names to be listed in `list_md_to_retrieve()` (<leader>ml)
-      -- types_to_retrieve = {
-      --   "ApexClass",
-      --   "ApexTrigger",
-      --   "StaticResource",
-      --   "LightningComponentBundle"
-      -- },
+      enable_hotkeys = false
     })
   end
 }
