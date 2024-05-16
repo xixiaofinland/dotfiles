3dca0add08a0fdf2c983b38c7c862a065da4df80
xi xiao <tdxiaoxi2@gmail.com>
2024-05-16 13:02:22 +0300

    +

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
     local harpoon = require("harpoon")
     harpoon:setup()
 
-    vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = name .. "append" })
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = name .. "add" })
     vim.keymap.set("n", "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
       { desc = name .. "list" })
 
