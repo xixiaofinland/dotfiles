73e3704ce44de260c9604215dc3798ac2fd3f620
xi xiao <tdxiaoxi2@gmail.com>
2024-05-16 17:23:59 +0300

    +

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -7,6 +7,14 @@ return {
     local harpoon = require("harpoon")
     harpoon:setup()
 
+    harpoon:extend({
+      UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-v>", function()
+          harpoon.ui:select_menu_item({ vsplit = true })
+        end, { buffer = cx.bufnr })
+      end,
+    })
+
     vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = name .. "add" })
     vim.keymap.set("n", "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
       { desc = name .. "list" })
