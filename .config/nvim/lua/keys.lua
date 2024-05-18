f0710cf143c4eb207af4d6f1e4227cb3a4898aba
xi xiao <tdxiaoxi2@gmail.com>
2024-05-18 19:47:25 +0300

    +

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -65,3 +65,23 @@ vim.keymap.set('n', '<leader>ct',
       end
     })
   end, { noremap = true, silent = true, desc = "generate ctag for Apex" })
+
+-- toggle ending ; and ,
+
+local toggle = function(character)
+  local api = vim.api
+  local delimiters = { ',', ';' }
+  local line = api.nvim_get_current_line()
+  local last_char = line:sub(-1)
+
+  if last_char == character then
+    return api.nvim_set_current_line(line:sub(1, #line - 1))
+  elseif vim.tbl_contains(delimiters, last_char) then
+    return api.nvim_set_current_line(line:sub(1, #line - 1) .. character)
+  else
+    return api.nvim_set_current_line(line .. character)
+  end
+end
+
+vim.keymap.set('n', '<leader>,', function() toggle(',') end, { noremap = true, silent = true, desc = 'toggle ","' })
+vim.keymap.set('n', '<leader>;', function() toggle(';') end, { noremap = true, silent = true, desc = 'toggle ";"' })
--- .config/nvim/lua/plugins/charttoggle.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'saifulapm/chartoggle.nvim',
-  config = function()
-    require('chartoggle').setup({
-      leader = '<localleader>', -- you can use any key as Leader
-      keys = { ',', ';' }       -- Which keys will be toggle end of the line
-    })
-  end
-}
