73cec8c3774902585b70b1d1461dbcc88ad5ed4e
xi xiao <tdxiaoxi2@gmail.com>
2024-05-27 19:17:14 +0300

    +

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,16 +1,15 @@
 return {
   {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {}
+  },
+  {
     "tris203/precognition.nvim",
     config = {
       startVisible = false,
     }
   },
-  {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {}
-  },
-
   vim.keymap.set('n', '\\k', function()
     require("precognition").toggle()
     -- vim.cmd('Hardtime toggle')
