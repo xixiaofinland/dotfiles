b675c77b8858c6d8ee65e890c4450d42cfd07a66
xi xiao <tdxiaoxi2@gmail.com>
2024-06-01 09:03:53 +0300

    +

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -9,6 +9,7 @@ return {
     },
     sections = {
       lualine_a = { 'mode' },
+      lualine_b = { 'branch' },
       -- lualine_b = { 'branch', 'diff', 'diagnostics' },
       lualine_c = { 'filename', {
         "require'sf'.get_target_org()",
