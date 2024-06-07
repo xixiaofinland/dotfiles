a870832c48b78b6be2471caac9416667a64c6a59
xi xiao <tdxiaoxi2@gmail.com>
2024-06-07 20:35:08 +0300

    +

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -2,7 +2,15 @@ return {
   'MeanderingProgrammer/markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
-    require('render-markdown').setup({})
+    require('render-markdown').setup({
+      headings = { '📙', '📒 ', '📗 ', '📓 ', '📄 ' },
+      bullets = { '📌 ', '✏️', '◆', '◇' },
+      highlights = {
+        heading = {
+          backgrounds = {},
+        }
+      }
+    })
     vim.keymap.set('n', '\\m', require('render-markdown').toggle, { desc = 'toggle markdown' })
   end,
 }
